da337bd3571350cef0d3c7440bb0577a79ea56f7
Darren Bishop <D_R_Bish@hotmail.com>
2016-04-26 23:05:18 +1000

    removed unecessary cycles

--- problem_3.c
+++ problem_3.c
@@ -3,8 +3,12 @@
 #define FALSE (1==0)
 #define TRUE  (1==1)
 
-int check_prime (int x){ 
-       for( int y = 2; y < x; y = y + 1 ){
+int check_prime (long long x){ 
+       printf("Checking: %llu\n", x);
+       if(x%2==0){
+           return FALSE;
+       }
+       for( int y = 3; y < x/2; y = y + 2 ){
            if(x%y==0){
                return FALSE;
            }
@@ -15,7 +19,11 @@ int check_prime (int x){
 
 
 int return_highest_prime_factor(long long x){
-    for( long long a = x-1; a < x; a = a - 1 ){
+    printf("Checking val: %llu\n", x);
+    for( long long a = x/2; a > 1; a = a - 2 ){
+        if(a%10000001==0){
+            printf("Testing factor: %llu\n", a);
+        }
         if(x%a==0 && check_prime(a)){
             return a;
         }
@@ -24,9 +32,9 @@ int return_highest_prime_factor(long long x){
 
 
 int main(){
-printf ("Checking primes...\n");
-long long val =600851475143LL; 
-long long largest = return_highest_prime_factor(val);
-printf("Largest prime factor is %llu\n", largest);
-return 0;
+    printf ("Checking primes...\n");
+    long long val = 600851475143LL; 
+    long long largest = return_highest_prime_factor(val);
+    printf("Largest prime factor is %llu\n", largest);
+    return 0;
 }
